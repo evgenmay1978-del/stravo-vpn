c48b85c1cedec1752a5b089e232dc238d31271ed
sztushino-debug <sztushino@gmail.com>
2026-09-15 23:21:48 +0300

    fix: remove invalid Compose weight import

--- app/src/main/java/com/stravo/vpn/ui/screens/common/StravoScreen.kt
+++ app/src/main/java/com/stravo/vpn/ui/screens/common/StravoScreen.kt
@@ -10,7 +10,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.weight
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
@@ -32,7 +31,6 @@ import androidx.compose.ui.unit.sp
 import com.stravo.vpn.ui.components.CompassMark
 import com.stravo.vpn.ui.components.StravoCard
 import com.stravo.vpn.ui.theme.StravoColors
-import com.stravo.vpn.ui.theme.StravoShapes
 import com.stravo.vpn.ui.theme.StravoTypography
 
 @Composable
